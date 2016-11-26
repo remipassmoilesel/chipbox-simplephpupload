183b8d6800cfe88430ba9cc523ffcffcd8ef00bb
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-11-26 19:21:49 +0100

    Update name

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 // Please limit your configuration to settings below.
 
 $settings['debug'] = false;
-$settings['title'] = "Chipbox upload";
+$settings['title'] = "Tor-sharing upload";
 $settings['description'] = "Upload files and share them !";
 $settings['allow_deletion'] = true;
 
