706243bbb22583efd49cf65e1b391b46c00e30ea
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-11-30 11:02:58 +0100

    Rename project

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 // Please limit your configuration to settings below.
 
 $settings['debug'] = false;
-$settings['title'] = "Tor-sharing upload";
+$settings['title'] = "Tor-share upload";
 $settings['description'] = "Upload files and share them !";
 $settings['allow_deletion'] = true;
 
